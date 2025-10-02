WKCW-Project/android_kernel_xiaomi_sm7435-modules

audio-kernel: Import fs19xx codec
 * from muyu-v-oss

Change-Id: I6d2fd85eca489b6b0f0b77f14a5199a8dba19617

qcom/opensource/audio-kernel/audio_kernel_product_board.mk
@@ -31,6 +31,7 @@ PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
 	$(KERNEL_MODULES_OUT)/wcd938x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/cs35l43_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/fs19xx_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/hdmi_dlkm.ko

qcom/opensource/audio-kernel/audio_kernel_vendor_board.mk
@@ -44,6 +44,7 @@ BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
 	$(KERNEL_MODULES_OUT)/wcd938x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/cs35l43_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/fs19xx_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/hdmi_dlkm.ko

Evolution-X-Devices/kernel_xiaomi_garnet-modules

qcom/opensource/audio-kernel/audio_kernel_product_board.mk
@@ -32,5 +32,6 @@ PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
 	$(KERNEL_MODULES_OUT)/wcd937x_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/hdmi_dlkm.ko \
-	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko
+	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/fs19xx_dlkm.ko
 

qcom/opensource/audio-kernel/audio_kernel_vendor_board.mk
@@ -45,7 +45,8 @@ BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
 	$(KERNEL_MODULES_OUT)/wcd937x_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd937x_slave_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/hdmi_dlkm.ko \
-	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko
+	$(KERNEL_MODULES_OUT)/aw882xx_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/fs19xx_dlkm.ko
 endif
 endif
 endif